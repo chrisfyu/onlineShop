0b8da71378797abaa5cff5daadddf2126e8536b9
chrisfyu <chrisfyu@hotmail.com>
2019-02-19 21:52:26 -0500

    add SalseOrder dao, service and controller

---
+++ src/main/java/onlineShop/service/SalesOrderServiceImpl.java
@@ -0,0 +1,12 @@
+package onlineShop.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import onlineShop.dao.SalesOrderDao;
+import onlineShop.model.SalesOrder;
+
+@Service
+public class SalesOrderServiceImpl implements SalesOrderService {
+
+}
